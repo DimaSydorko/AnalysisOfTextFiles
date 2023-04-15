384d6a9e6d0d630c0b232a6cc9b6baa70268399a
DmytroSydorko <sydorko23@gmail@com>
2023-04-15 19:59:49 +0300

    Update security rules

--- AnalysisOfTextFiles/State/AdminSettings.cs
+++ AnalysisOfTextFiles/State/AdminSettings.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.IO;
+using System.Security.Cryptography;
 using System.Security.Principal;
 using System.Text;
 using System.Windows;
 
 public class AdminSettings
 {
-  private static string iniFilePath = "styleSettings.ini";
-  
+  private static string settingsFilePath = "styleSettings.ini";
+  private static string hashFilePath = "hash.ini";
+
   public static string GetStyleSettings()
   {
+    string iniData = null;
     string iniDataHash = null;
 
     try
     {
-      iniDataHash = File.ReadAllText(iniFilePath);
+      iniData = File.ReadAllText(settingsFilePath);
+      iniDataHash = File.ReadAllText(hashFilePath);
     }
     catch (Exception ex)
     {
@@ -22,11 +26,17 @@ public class AdminSettings
       return "";
     }
 
-    if (iniDataHash != null)
+    bool isDataDamaged = !string.Equals(iniDataHash, EncodeDataToHash(iniData));
+
+    if (isDataDamaged)
     {
-      return DecodeHash(iniDataHash);
+      MessageBox.Show("Settings file was damaged", "Error");
+
+      bool isAdmin = IsUserAdmin();
+      if (isAdmin) return "";
     }
-    return "";
+    
+    return iniData;
   }
 
   public static void SetStyleSettings(string decData)
@@ -40,30 +50,23 @@ public class AdminSettings
     string? encData = EncodeDataToHash(decData);
     if (encData != null)
     {
-      File.WriteAllText(iniFilePath, encData);
+      File.WriteAllText(settingsFilePath, decData);
+      File.WriteAllText(hashFilePath, encData);
 
       MessageBox.Show("INI file updated successfully.", "Success");
     }
   }
-  
+
   public static string EncodeDataToHash(string dataToEncode)
   {
-    var plainTextBytes = Encoding.UTF8.GetBytes(dataToEncode);
-    return Convert.ToBase64String(plainTextBytes);
-  }
-
-  public static string DecodeHash(string hashedData)
-  {
-    try
+    // var plainTextBytes = Encoding.UTF8.GetBytes(dataToEncode);
+    // return Convert.ToBase64String(plainTextBytes);
+    byte[] dataBytes = Encoding.UTF8.GetBytes(dataToEncode);
+    using (SHA256 sha256 = SHA256.Create())
     {
-    byte[] base64EncodedBytes = Convert.FromBase64String(hashedData);
-    string decodedData = Encoding.UTF8.GetString(base64EncodedBytes);
-    return decodedData;
-    }
-    catch (Exception ex)
-    {
-      MessageBox.Show($"Encoding: {ex.Message}", "Error");
-      return null;
+      byte[] hashBytes = sha256.ComputeHash(dataBytes);
+      string hashedData = Convert.ToBase64String(hashBytes);
+      return hashedData;
     }
   }
 
